3b794c193ef908a6e8854ed7f889bde7d13ebb98
Mike Maschek <mmaschek@gmail.com>
2017-03-04 00:03:44 -0600

    add page in progress

---
+++ StuffedAnimalCollector/StuffedAnimalViewController.swift
@@ -0,0 +1,31 @@
+//
+//  StuffedAnimalViewController.swift
+//  StuffedAnimalCollector
+//
+//  Created by Mike Maschek on 3/3/17.
+//  Copyright © 2017 Mike Maschek. All rights reserved.
+//
+
+import UIKit
+
+class StuffedAnimalViewController: UIViewController {
+
+    @IBOutlet weak var animalImageView: UIImageView!
+    
+    @IBOutlet weak var stuffedAnimalTextField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func photosTapped(_ sender: Any) {
+    }
+
+    @IBAction func cameraTapped(_ sender: Any) {
+    }
+
+    @IBAction func addTapped(_ sender: Any) {
+    }
+}
