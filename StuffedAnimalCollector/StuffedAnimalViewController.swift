012e583bce2d4f56671b8e359d7a12244cb7985d
Mike Maschek <mmaschek@gmail.com>
2017-03-16 07:32:24 -0500

    create stuff animal objects and save data

--- StuffedAnimalCollector/StuffedAnimalViewController.swift
+++ StuffedAnimalCollector/StuffedAnimalViewController.swift
@@ -8,24 +8,43 @@
 
 import UIKit
 
-class StuffedAnimalViewController: UIViewController {
+class StuffedAnimalViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
     @IBOutlet weak var animalImageView: UIImageView!
-    
     @IBOutlet weak var stuffedAnimalTextField: UITextField!
+    
+    var imagePicker = UIImagePickerController()
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        imagePicker.delegate = self
     }
 
     @IBAction func photosTapped(_ sender: Any) {
+        imagePicker.sourceType = .photoLibrary
+        present(imagePicker, animated: true, completion: nil)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        let image = info[UIImagePickerControllerOriginalImage] as! UIImage
+        
+        animalImageView.image = image
+        imagePicker.dismiss(animated: true, completion: nil)
     }
 
     @IBAction func cameraTapped(_ sender: Any) {
     }
 
     @IBAction func addTapped(_ sender: Any) {
+        let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+        let stuffedAnimal = StuffedAnimal(context: context)
+        
+        stuffedAnimal.name = stuffedAnimalTextField.text
+        stuffedAnimal.image = UIImagePNGRepresentation(animalImageView.image!) as NSData?
+        
+        (UIApplication.shared.delegate as! AppDelegate).saveContext()
+        
+        navigationController?.popViewController(animated: true)
     }
 }
--- StuffedAnimalCollector/ViewController.swift
+++ StuffedAnimalCollector/ViewController.swift
@@ -8,18 +8,44 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
+    @IBOutlet weak var tableView: UITableView!
+    
+    var stuffedAnimals : [StuffedAnimal] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        tableView.dataSource = self
+        tableView.delegate = self
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    override func viewWillAppear(_ animated: Bool) {
+        let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+        
+        do {
+            try stuffedAnimals = context.fetch(StuffedAnimal.fetchRequest())
+            print(stuffedAnimals)
+            tableView.reloadData()
+        }
+        catch {
+            
+        }
     }
-
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return stuffedAnimals.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        let stuffedAnimal = stuffedAnimals[indexPath.row]
+        cell.textLabel?.text = stuffedAnimal.name
+        cell.imageView?.image = UIImage(data: stuffedAnimal.image! as Data)
+        return cell
+    }
 
 }
 
